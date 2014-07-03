c55e1d7ffc6bf3b9357cdd02d76a650f6cde1249
Claudius Link <claudius.link@googlemail.com>
2014-07-04 00:40:12 +0200

    Added the number of header files to demonstrate what the prepocessor does.

--- 00-Basic-Language-Features/01-hello-main.cpp
+++ 00-Basic-Language-Features/01-hello-main.cpp
@@ -6,7 +6,11 @@
 //          g++ -E 01-hello-main.cpp   | wc 
 //             37402  102779 1206796
 //      Meaning near 40k lines, ~1.2Mb
+//          g++ -E 01-hello-main.cpp | grep "# 1 " | cut -d " " -f 3 | sort -u | wc -l
+//               188
+//      over 180 included headers
 //      On Mac OS 10.9.4, Apple LLVM version 5.1 (clang-503.0.40) (based on LLVM 3.4svn)
+//      /usr/include/c++/4.2.1
 #include <iostream>
 
 int main() 
