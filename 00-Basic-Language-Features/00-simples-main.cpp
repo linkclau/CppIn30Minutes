75af50375fae2562904318259fc580a08211a4ce
Claudius Link <claudius.link@googlemail.com>
2014-07-04 00:37:51 +0200

    Added hte topics.

--- 00-Basic-Language-Features/00-simples-main.cpp
+++ 00-Basic-Language-Features/00-simples-main.cpp
@@ -2,6 +2,8 @@
 // main() is a function which "returns" and integer.
 // Per convertion the (usual) environemnt uses the main() function as entry point. 
 // (Oh, and it is the smalles C program too)
+//
+// Topic: types, functions, scope (blocks)
 // 
 // Reference: 3.6.1 Main function [basic.start.main]
 // Standard for Programming Language C++ n3337
