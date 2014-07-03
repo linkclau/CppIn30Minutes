7c2ddf6ffdb070867ab82f01fc1ea2d477d922a8
Claudius Link <claudius.link@googlemail.com>
2014-07-03 17:44:43 +0200

    Simplest possible C++ programm with some explanations.

---
+++ 00-Basic-Language-Features/00-simples-main.cpp
@@ -0,0 +1,7 @@
+// The simplest standard conform C++ program.
+// main() is a function which "returns" and integer.
+// Per convertion the (usual) environemnt uses the main() function as entry point. 
+// 
+// Reference: 3.6.1 Main function [basic.start.main]
+// Standard for Programming Language C++ n3337
+int main() {}
