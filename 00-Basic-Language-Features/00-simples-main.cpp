ddb1970247e89208594decc6f52d54790b859fb9
Claudius Link <claudius.link@googlemail.com>
2014-07-03 22:36:48 +0200

    Empty main() is the smalles legal C program too

--- 00-Basic-Language-Features/00-simples-main.cpp
+++ 00-Basic-Language-Features/00-simples-main.cpp
@@ -1,6 +1,7 @@
 // The simplest standard conform C++ program.
 // main() is a function which "returns" and integer.
 // Per convertion the (usual) environemnt uses the main() function as entry point. 
+// (Oh, and it is the smalles C program too)
 // 
 // Reference: 3.6.1 Main function [basic.start.main]
 // Standard for Programming Language C++ n3337
