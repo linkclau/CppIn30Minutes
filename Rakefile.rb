8fcd958a3e5390b33de536d3e0781e0a4ec91e1f
Claudius Link <claudius.link@googlemail.com>
2014-07-03 23:27:40 +0200

    Build and clean all executable (*-main*) and objects (*.cpp -> *.o)

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,18 +1,24 @@
-
+require 'rake/clean'
 
 sources = FileList['**/*.cpp']
 
-objects = [] 
-sources.each{|cpp|
-  object = cpp.sub( /\.cpp$/, '.o' )
-  puts "#{cpp} #{object}"
-  objects << object
-  file object => [ cpp ] {|t|
+objects = sources.map{|s| s.sub('.cpp', '.o') }
+CLEAN << objects
+objects.each{|o|
+  file o => [ o.sub( '.o', '.cpp' )  ] {|t|
     cmd = "g++ -Wall  -Weverything -pedantic -c -o #{t.name} #{t.prerequisites.join(' ')}"
     sh cmd
   }
 }
 
+executables = FileList['**/*-main*.cpp'].map{|e| e.sub('.cpp', '') }
+CLEAN << executables
+executables.each{|e|
+  file e => [ e + '.o' ] {|t|
+    cmd = "g++ -Wall  -Weverything -pedantic -o #{t.name} #{t.prerequisites.join(' ')}"
+    sh cmd
+  }
+}
 
 
-task :default => objects
+task :default => executables + objects
