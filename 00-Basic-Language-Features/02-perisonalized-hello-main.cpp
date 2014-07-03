60ac3b9cc7b85b774ff481c5231e757f8820f725
Claudius Link <claudius.link@googlemail.com>
2014-07-04 00:41:29 +0200

    Configurable with variables

---
+++ 00-Basic-Language-Features/02-perisonalized-hello-main.cpp
@@ -0,0 +1,20 @@
+// Danamic greeting
+//
+// Topic: variables, string, using, conditionals (if), function arguments, char, pointers
+//
+// Note: Pre-processed size same as before
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+    string greetingTarget("World");
+
+    if( argc>1) {
+        greetingTarget = argv[1];
+    }
+
+    cout << "Hello " << greetingTarget << "!" << endl;
+}
