f5e52621616181a2451a77eca82f42cca5a2d0dd
Claudius Link <claudius.link@googlemail.com>
2014-07-04 00:53:58 +0200

    Enforce C++11 and don't emit all warnings when using C++11 features.

--- Rakefile.rb
+++ Rakefile.rb
@@ -2,11 +2,13 @@ require 'rake/clean'
 
 sources = FileList['**/*.cpp']
 
+CXX_FLAGS = "-Wall  -Weverything -pedantic -std=c++11 -Wno-c++11-extensions"
+
 objects = sources.map{|s| s.sub('.cpp', '.o') }
 CLEAN << objects
 objects.each{|o|
   file o => [ o.sub( '.o', '.cpp' )  ] {|t|
-    cmd = "g++ -Wall  -Weverything -pedantic -c -o #{t.name} #{t.prerequisites.join(' ')}"
+    cmd = "g++ #{CXX_FLAGS} -c -o #{t.name} #{t.prerequisites.join(' ')}"
     sh cmd
   }
 }
@@ -15,7 +17,7 @@ executables = FileList['**/*-main*.cpp'].map{|e| e.sub('.cpp', '') }
 CLEAN << executables
 executables.each{|e|
   file e => [ e + '.o' ] {|t|
-    cmd = "g++ -Wall  -Weverything -pedantic -o #{t.name} #{t.prerequisites.join(' ')}"
+    cmd = "g++ #{CXX_FLAGS} -o #{t.name} #{t.prerequisites.join(' ')}"
     sh cmd
   }
 }
